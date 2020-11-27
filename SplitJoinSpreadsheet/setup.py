609d565ec3adc356f91af726a514176a4aba3cc8
Sushovan <sushovan86@gmail.com>
2020-11-27 20:26:03 +0530

    Error message changed
    Signed-off-by: Sushovan <sushovan86@gmail.com>

--- SplitJoinSpreadsheet/setup.py
+++ SplitJoinSpreadsheet/setup.py
@@ -4,7 +4,7 @@ setuptools.setup(
     name='SplitJoinSpreadsheet',
     version='1.0',
     packages=setuptools.find_packages(),
-    url='',
+    url='https://github.com/sushovan86/PythonProjects.git',
     license='',
     author='Sushovan Chakraborty',
     author_email='sushovan86@gmail.com',
